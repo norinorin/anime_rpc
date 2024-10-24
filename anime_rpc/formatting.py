99baec8a52a209c3d72ca3c89a40308fd5db3ded
Norizon <norizontunes@gmail.com>
2024-10-25 01:31:16 +0700

    Add credit

--- anime_rpc/formatting.py
+++ anime_rpc/formatting.py
@@ -1,4 +1,5 @@
-ranges = [
+# https://stackoverflow.com/questions/30069846/how-to-find-out-chinese-or-japanese-character-in-a-string-in-python
+CJK_RANGES = [
     {"from": ord("\u3300"), "to": ord("\u33ff")},  # compatibility ideographs
     {"from": ord("\ufe30"), "to": ord("\ufe4f")},  # compatibility ideographs
     {"from": ord("\uf900"), "to": ord("\ufaff")},  # compatibility ideographs
@@ -16,7 +17,7 @@ ranges = [
 
 
 def _is_char_cjk(char: str) -> bool:
-    return any([range["from"] <= ord(char) <= range["to"] for range in ranges])
+    return any([range["from"] <= ord(char) <= range["to"] for range in CJK_RANGES])
 
 
 def _contains_cjk(text: str) -> bool:
