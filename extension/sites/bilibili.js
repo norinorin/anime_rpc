90239b67154c566474599df64baa2e65163970ca
Norizon <norizontunes@gmail.com>
2024-10-29 15:19:15 +0700

    Add bilibili's episode_title

--- extension/sites/bilibili.js
+++ extension/sites/bilibili.js
@@ -8,11 +8,11 @@ function getStateFromBilibili() {
   if (!window.document.querySelector(".bstar-meta-tag--anime")) return;
 
   let title = window.document.querySelector(".bstar-meta__title")?.textContent;
-  let episode = window.document
-    .querySelector(".ep-item--active")
-    ?.textContent.match(/\d+/)[0];
+  let epElement = window.document.querySelector(".ep-item--active");
+  let episode = epElement?.textContent.match(/\d+/)[0];
+  let episode_title = epElement?.title;
 
   if (!episode) return;
 
-  return { title, episode, videoElement };
+  return { title, episode_title, episode, videoElement };
 }
