e647f32d932b0a825cbb74a6b7156753142f09af
Nori <norizontunes@gmail.com>
2025-03-17 21:40:00 +0700

    check if filename is absolute or relative

--- anime_rpc/pollers/mpv_poller.py
+++ anime_rpc/pollers/mpv_poller.py
@@ -1,9 +1,12 @@
+import os
+
 import aiohttp
 
 from anime_rpc.pollers.base_poller import BasePoller, Vars
 from anime_rpc.states import WatchingState
 
 
+# maybe consider using native socket instead of simple-mpv-webui?
 class MPVPoller(BasePoller):
     port = 14567  # TODO: allow for custom ports via cli
 
@@ -21,9 +24,17 @@ class MPVPoller(BasePoller):
                     return None
 
                 data = await response.json()
+
+                # depending on how you spawn mpv, the filename may be relative or absolute
+                # filename is absolute if you activate a video file in thunar for example.
+                full_path = data["filename"]
+
+                if not os.path.isabs(full_path):
+                    full_path = os.path.join(data["working-dir"], full_path)
+
                 return Vars(
-                    file=data["filename"],
-                    filedir=data["working-dir"],
+                    file=os.path.basename(full_path),
+                    filedir=os.path.dirname(full_path),
                     state=WatchingState.PLAYING
                     if not data["pause"]
                     else WatchingState.PAUSED,
