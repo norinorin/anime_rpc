13eab6937f5c6349e4965a2544e362057b92dee7
Nori <norizontunes@gmail.com>
2025-04-10 16:06:50 +0700

    use Git commit hash as __version__ identifier

--- anime_rpc/__init__.py
+++ anime_rpc/__init__.py
@@ -1,2 +1,17 @@
+import contextlib
+import shutil
+import subprocess
+
 __author__ = "norinorin"
-__version__ = "1.1.4"
+__version__ = "unknown"
+
+with contextlib.suppress(subprocess.CalledProcessError, FileNotFoundError, OSError):
+    _git_path = shutil.which("git")
+    if _git_path:
+        __version__ = (
+            subprocess.check_output(  # noqa: S603
+                [_git_path, "rev-parse", "--short", "HEAD"],
+            )
+            .decode("ascii")
+            .strip()
+        )
