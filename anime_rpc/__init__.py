84e7fe12373d7094ca6d1f9ec39fe14d60128132
Nori <norizontunes@gmail.com>
2025-04-25 00:57:38 +0700

    metadata: add branch name to version

--- anime_rpc/__init__.py
+++ anime_rpc/__init__.py
@@ -8,10 +8,18 @@ __version__ = "unknown"
 with contextlib.suppress(subprocess.CalledProcessError, FileNotFoundError, OSError):
     _git_path = shutil.which("git")
     if _git_path:
-        __version__ = (
+        commit_hash = (
             subprocess.check_output(  # noqa: S603
                 [_git_path, "rev-parse", "--short", "HEAD"],
             )
             .decode("ascii")
             .strip()
         )
+        branch_name = (
+            subprocess.check_output(  # noqa: S603
+                [_git_path, "rev-parse", "--abbrev-ref", "HEAD"],
+            )
+            .decode("ascii")
+            .strip()
+        )
+        __version__ = f"{branch_name}@{commit_hash}"
