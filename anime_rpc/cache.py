88587f2cd306001f99f2ff4789316f30be99d125
Nori <norizontunes@gmail.com>
2025-05-05 00:51:18 +0700

    get rid of trailing slashes in cache paths

--- anime_rpc/cache.py
+++ anime_rpc/cache.py
@@ -7,5 +7,5 @@ from anime_rpc import __author__
 __all__: tuple[str, ...] = ("BASE_CACHE_DIR", "SCRAPING_CACHE_DIR")
 
 BASE_CACHE_DIR = Path(user_cache_dir("anime_rpc", __author__))
-SCRAPING_CACHE_DIR = BASE_CACHE_DIR / "scraped/"
+SCRAPING_CACHE_DIR = BASE_CACHE_DIR / "scraped"
 SCRAPING_CACHE_DIR.mkdir(parents=True, exist_ok=True)
