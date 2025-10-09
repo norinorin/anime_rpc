6fc7b143cc03f1f8adc2b654ecd88bfbe5f2a927
Norizon <norizontunes@gmail.com>
2025-10-10 01:10:23 +0700

    build: only embed platform-specific library

--- build.sh
+++ build.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+case "$(uname -s)" in
+    Linux*)
+        LIB_NAME="libdiscord_partner_sdk.so"
+        ;;
+    Darwin*)
+        LIB_NAME="libdiscord_partner_sdk.dylib"
+        ;;
+    CYGWIN*|MINGW32*|MSYS*|MINGW*)
+        LIB_NAME="discord_partner_sdk.dll"
+        ;;
+    *)
+        echo "Unsupported platform: $(uname -s)"
+        exit 1
+        ;;
+esac
+
+echo "Detected platform: $(uname -s), using library: $LIB_NAME"
+
 # trigger setuptools-scm
 # FIXME: write a little script that writes to 
 # anime_rpc/_version.py instead so we don't have 
@@ -8,4 +26,4 @@ pip install -e .
 
 # warning: --optimize 2 breaks cffi
 # FIXME: only include relevant library for the platform
-pyinstaller --onefile --name anime_rpc --optimize 1 --add-binary "./lib/*:lib" --add-binary "./include/*:include" launcher.py
+pyinstaller --onefile --name anime_rpc --optimize 1 --add-binary "./lib/$LIB_NAME:lib" --add-binary "./include/*:include" launcher.py
