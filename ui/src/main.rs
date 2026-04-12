d40e2d5374fce4915b4ce17e8f8d3f79d5277aac
Nori <norizon16@proton.me>
2026-04-12 18:02:18 +0700

    ui: hide on startup

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -12,10 +12,12 @@ use iced::{Size, window};
 pub fn main() -> iced::Result {
     iced::application(AnimeRpc::init, AnimeRpc::update, AnimeRpc::view)
         .subscription(AnimeRpc::subscription)
+        .antialiasing(true)
         .title("Anime RPC")
         .window(window::Settings {
             size: Size::new(constants::WINDOW_WIDTH, constants::WINDOW_HEIGHT),
             resizable: false,
+            visible: false,
             ..Default::default()
         })
         .theme(|_state: &AnimeRpc| iced::Theme::Dark)
